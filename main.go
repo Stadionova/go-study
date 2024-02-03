06865a991c4be3b6cc3017ee194e6e0903864dc8
julia tribunskaja <jtribunskaja@hamkorlab.club>
2024-02-03 14:50:26 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+
+import "fmt"
+
+
+// 1 EXAMPLE
+// func main() {
+//     fmt.Println("Hello, Go!")
+// }
+
+
+
+// 2 EXAMPLE
+// func main() {
+
+//     var hello string
+
+//     hello = "Hello Go!"
+
+//     var a int = 2019
+
+//     fmt.Println(hello)
+//     fmt.Println(a)
+
+// 	var symbol int32 = 'c'
+// 	fmt.Println(string(symbol))
+
+// }
+
+
+
+// 3 EXAMPLE
+func main() {
+    var (
+        name string = "Dima"
+        age int = 23
+    )
+     
+    fmt.Println(name)
+    fmt.Println(age)
+}
